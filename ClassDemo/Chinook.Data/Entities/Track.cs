7875dd5fe4ab233eb3dcf54dfd405a44eaff6abd
ricofunghl <hfung4@nait.ca>
2017-02-06 19:09:23 -0700

    ListView ODS, validations

--- ClassDemo/Chinook.Data/Entities/Track.cs
+++ ClassDemo/Chinook.Data/Entities/Track.cs
@@ -16,11 +16,20 @@ namespace Chinook.Data.Entities
     {
         [Key]
         public int TrackId { get; set; }
+
+        [Required(ErrorMessage ="Name is a required field")]
+        [StringLength(200,ErrorMessage = "Name is limited to 200 characters")]
         public string Name { get; set; }
         public int? AlbumId { get; set; }
         public int MediaTypeId { get; set; }
         public int? GenreId { get; set; }
+
+        [StringLength(220, ErrorMessage ="Composer is limited to 220 characters")]
         public string Composer { get; set; }
+
+        [Required(ErrorMessage ="Millisecond is a required fileld")]
+        [Range(1.0, double.MaxValue, ErrorMessage ="Millisecond value out of range; must be greater than 0.")]
+
         public int Milliseconds { get; set; }
         public int? Bytes { get; set; }
         public decimal UnitPrice { get; set; }
